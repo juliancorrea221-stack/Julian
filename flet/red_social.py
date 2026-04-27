606ec188d5b7471399c085950a251575a74e0a1a
juliancorrea221-stack <juliancorrea221@gmail.com>
2026-04-27 14:09:17 -0500

    grupos

--- flet/red_social.py
+++ flet/red_social.py
@@ -11,6 +11,23 @@ class red_social:
         self.color_primaria = "black"
         self.color_secundaria = "green800"
         self.result_api = ft.Column(horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+        self.grupo_actual = None
+        self.grupos_disponibles = [
+        {"nombre": "Fitness Hardcore", "miembros": 3, "capacidad": 5},
+        {"nombre": "Programadores Python", "miembros": 5, "capacidad": 5},
+        {"nombre": "Gamers Colombia", "miembros": 2, "capacidad": 5},
+        {"nombre": "Lectura diaria", "miembros": 1, "capacidad": 5},
+            ]
+        self.usuario = {
+        "nombre": "Tú",
+        "puntos": 0,
+        "retos_completados": 0
+        }
+        self.retos = [
+        {"nombre": "Entrenar 1 hora", "puntos": 10},
+        {"nombre": "Leer 20 páginas", "puntos": 5},
+        {"nombre": "Despertar 5AM", "puntos": 15},
+        ]
         
         # Iniciamos con el Login
         self.mostrar_login()
@@ -44,7 +61,8 @@ class red_social:
             ft.Text("Inicia sesión para continuar", color="white"),
             self.user_tf,
             self.pass_tf,
-            ft.ElevatedButton("Entrar", on_click=validar, bgcolor="green800", color="white")
+            ft.ElevatedButton("Entrar", on_click=validar, bgcolor="green800", color="white"),
+             
         )
         
         # Forzamos la actualización
@@ -135,10 +153,170 @@ class red_social:
         ], spacing=20)
 
     def build_retos(self):
-        return ft.Column([ft.Text("Desafios", size=30, weight="bold", color="white")])
+        lista = ft.Column()
+
+        def completar(reto):
+            self.usuario["puntos"] += reto["puntos"]
+            self.usuario["retos_completados"] += 1
+
+            self.page.snack_bar = ft.SnackBar(
+                ft.Text(f"+{reto['puntos']} puntos")
+            )
+            self.page.snack_bar.open = True
+
+            self.page.update()
+
+        for reto in self.retos:
+            lista.controls.append(
+                ft.Row([
+                    ft.Text(reto["nombre"], color="white"),
+                    ft.ElevatedButton(
+                        "Completar",
+                        on_click=lambda e, r=reto: completar(r),
+                        bgcolor="green"
+                    )
+                ], alignment="spaceBetween")
+            )
+
+        return ft.Column([
+            ft.Text("Retos diarios", size=30, color="white"),
+            lista
+        ])
 
     def build_grupos(self):
-        return ft.Column([ft.Text("Gremios", size=30, weight="bold", color="white")])
+        self.lista_grupos = ft.Column()
+
+        def buscar(e):
+            texto = self.buscador.value.lower()
+            self.lista_grupos.controls.clear()
+
+
+            for grupo in self.grupos_disponibles:
+                if texto in grupo["nombre"].lower():
+
+                    lleno = grupo["miembros"] >= grupo["capacidad"]
+
+                    self.lista_grupos.controls.append(
+                        ft.Row([
+                            ft.Column([
+                                ft.Text(grupo["nombre"], color="white"),
+                                ft.Text(
+                                    f'{grupo["miembros"]}/{grupo["capacidad"]} miembros',
+                                    color="white",
+                                    size=12
+                                )
+                            ]),
+                            ft.ElevatedButton(
+                                "Lleno" if lleno else "Unirse",
+                                disabled=lleno,
+                                bgcolor="grey" if lleno else "green",
+                                on_click=lambda e, g=grupo: unirse(g)
+                            )
+                        ], alignment="spaceBetween")
+                    )
+
+            self.page.update()
+
+        def unirse(grupo):
+            if grupo["miembros"] < grupo["capacidad"]:
+                grupo["miembros"] += 1
+                self.grupo_actual = grupo  # 👈 guardamos el grupo
+
+                self.page.snack_bar = ft.SnackBar(
+                    ft.Text(f"Entraste a {grupo['nombre']}")
+                )
+                self.page.snack_bar.open = True
+
+                # 👇 CAMBIAMOS AUTOMÁTICAMENTE A LA VISTA DEL GRUPO
+                self.abrir_chat_grupo()
+
+            else:
+                self.page.snack_bar = ft.SnackBar(
+                    ft.Text("Este grupo ya está lleno")
+                )
+                self.page.snack_bar.open = True
+
+            self.page.update()
+
+        self.buscador = ft.TextField(
+            label="Buscar grupos",
+            border_color="green",
+            color="white",
+            on_change=buscar
+        )
+
+        return ft.Column([
+            ft.Text("Gremios", size=30, weight="bold", color="white"),
+            self.buscador,
+            self.lista_grupos
+        ], spacing=20)
+    
+    def abrir_chat_grupo(self):
+        self.page.clean()
+        ranking = ft.Column()
+
+        def actualizar_ranking():
+            ranking.controls.clear()
+
+            # Simulación de otros usuarios
+            jugadores = [
+                {"nombre": "Ana", "puntos": 30},
+                {"nombre": "Luis", "puntos": 20},
+                self.usuario
+            ]
+
+            jugadores_ordenados = sorted(jugadores, key=lambda x: x["puntos"], reverse=True)
+
+            for i, j in enumerate(jugadores_ordenados):
+                ranking.controls.append(
+                    ft.Text(f"{i+1}. {j['nombre']} - {j['puntos']} pts", color="white")
+                )
+        mensajes = ft.Column(scroll=ft.ScrollMode.AUTO, expand=True)
+
+        input_mensaje = ft.TextField(
+            hint_text="Escribe un mensaje...",
+            expand=True,
+            color="white"
+        )
+
+        def enviar(e):
+            if input_mensaje.value.strip() != "":
+                mensajes.controls.append(
+                    ft.Text(f"Tú: {input_mensaje.value}", color="white")
+                )
+                input_mensaje.value = ""
+                actualizar_ranking()
+                self.page.update()
+
+        self.page.add(
+            ft.Column([
+                ft.Container(
+                    padding=15,
+                    bgcolor=self.color_secundaria,
+                    content=ft.Row([
+                        ft.Text(self.grupo_actual["nombre"], color="white", size=20),
+                        ft.TextButton("Salir", on_click=lambda _: self.volver_inicio(), style=ft.ButtonStyle(color="white"))
+                    ], alignment="spaceBetween")
+                ),
+                ft.Text("Ranking del grupo", color="green", size=18),
+                ranking,  # 👈 AQUI LO AGREGAS
+
+                ft.Divider(color="white"),
+
+                mensajes,
+
+                ft.Row([
+                    input_mensaje,
+                    ft.IconButton(
+                        icon=ft.Icons.SEND,
+                        on_click=enviar,
+                        icon_color="green"
+                    )
+                ])
+            ], expand=True)
+        )
+              
+        self.page.update()
 
     def build_perfil(self):
         return ft.Column([
